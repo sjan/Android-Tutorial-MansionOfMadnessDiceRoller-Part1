0b2213b5faf4d855c519b864ba8baaf9a0fcd51e
Stephen Jan <sjan828@outlook.com>
2018-04-07 17:53:11 -0400

    Initial Android Project Commit

---
+++ app/src/main/java/farsight/solutions/tutorial/diceroller/MainActivity.java
@@ -0,0 +1,13 @@
+package farsight.solutions.tutorial.diceroller;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
