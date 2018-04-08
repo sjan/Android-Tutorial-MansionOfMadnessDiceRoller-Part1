2743cb1689d321ff74ab335d9b8e7ff561b76c1b
Stephen Jan <sjan828@outlook.com>
2018-04-08 16:40:43 -0400

    -Adding click listener and logic for add, remove, and roll dice -Adding representation for dice [Blank, Magnify, Star] -Refactor adapter to keep a reference to the dice list in the activity.

--- app/src/main/java/farsight/solutions/tutorial/diceroller/MainActivity.java
+++ app/src/main/java/farsight/solutions/tutorial/diceroller/MainActivity.java
@@ -8,23 +8,54 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.ImageView;
 import android.widget.ListView;
 import android.widget.TextView;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Random;
 
 public class MainActivity extends AppCompatActivity {
+    DiceAdapter diceAdapter;
+    List <Dice> diceList = new ArrayList<>();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        //Connecting activity to layout
         setContentView(R.layout.activity_main);
 
+        //Setup ListView and Adapter
         ListView listView = findViewById(R.id.dice_list);
-        DiceAdapter diceAdapter = new DiceAdapter(this, R.layout.dice_row, new ArrayList<Dice>());
+        diceAdapter = new DiceAdapter(this, R.layout.dice_row, diceList);
         listView.setAdapter(diceAdapter);
+
+        //Initialize Data
         diceAdapter.add(new Dice());
+    }
+
+    public void rollDice(View view) {
+        //roll all dice
+        for(Dice dice : diceList) {
+            if(!dice.hold)
+                dice.roll();
+        }
+
+        //notify adapter to update view
+        diceAdapter.notifyDataSetChanged();
+    }
+
+    public void addDice(View view) {
+        diceAdapter.add(new Dice());
+    }
+
+    public void removeDice(View view) {
+        int index = diceList.size()-1;
+        if(index >= 0) {
+            diceAdapter.remove(diceAdapter.getItem(index));
+        }
     }
 
     public class DiceAdapter extends ArrayAdapter<Dice> {
@@ -33,19 +64,71 @@ public class MainActivity extends AppCompatActivity {
         }
 
         @Override
-        public View getView(int position, View convertView, ViewGroup parent) {
+        public View getView(final int position, View convertView, ViewGroup parent) {
             if (convertView == null) {
                 convertView = LayoutInflater.from(getContext()).inflate(R.layout.dice_row, parent, false);
             }
 
+            //setup dice label
             TextView textView = convertView.findViewById(R.id.dice_number);
             textView.setText(Integer.toString(position));
+
+            //setup dice image
+            ImageView imageView = convertView.findViewById(R.id.dice_icon);
+            final Dice dice = diceAdapter.getItem(position);
+            switch (dice.getValue()) {
+                case BLANK:
+                    imageView.setImageResource(R.drawable.blank_dice);
+                    break;
+                case MAGNIFY:
+                    imageView.setImageResource(R.drawable.magnifying_glass);
+                    break;
+                case STAR:
+                    imageView.setImageResource(R.drawable.star);
+                    break;
+            }
+
+            //setup dice hold button
+            Button button = convertView.findViewById(R.id.dice_hold_button);
+            button.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    Dice dice = diceList.get(position);
+                    dice.toggleHold();
+                }
+            });
 
             return convertView;
         }
     }
 
-    public class Dice {
+    public static class Dice {
+        boolean hold = false;
+        public Type value;
 
+        public enum Type {
+            BLANK,
+            MAGNIFY,
+            STAR
+        }
+
+        public static Random random = new Random();
+
+        Dice() {
+            roll();
+        }
+
+        public void roll() {
+            int num = random.nextInt(Type.values().length);
+            this.value = Type.values()[num];
+        }
+
+        public Type getValue() {
+            return value;
+        }
+
+        public void toggleHold() {
+            hold = !hold;
+        }
     }
 }
